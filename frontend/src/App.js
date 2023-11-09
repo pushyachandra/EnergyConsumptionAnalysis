4c7996e8ac3c83e8ba2688a6a1d219f6fd66342a
pushyachandra <pushyachandra.leburu@sjsu.edu>
2023-11-09 13:30:39 -0800

    Created a connection to Database(from Node to MySQL DB), Created a template environment, for the front-end development(Created Routes, Topbar, Sidebar)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
 import './App.css';
+
+import TopBar from './components/TopBar/TopBar';
+import SideBar from './components/SideBar/SideBar';
+import Sample from './components/Sample/Sample';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       wow
     </div>
+=======
+    <BrowserRouter>
+      <div className="topbar">
+        <TopBar />
+      </div>
+      <div className="sidebar">
+        <SideBar />
+      </div>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={ <Sample/> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 }
 
