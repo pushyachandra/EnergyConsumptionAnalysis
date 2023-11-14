5bba7d00c1353cfe3754bbc4002129a7430b3af3
pushyachandra <pushyachandra.leburu@sjsu.edu>
2023-11-14 12:28:01 -0800

    Analysis Dashboard page completion

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const multer = require('multer');
 const { Storage } = require('@google-cloud/storage');
 const { exec } = require('child_process');
+const moment = require('moment-timezone');
 
 const app = express();
 app.use(express.json());
@@ -58,14 +59,14 @@ app.post("/api/signUpUser",(req,res)=>{
 });
 
 app.get('/api/loginUser', (req,res) => {
-    console.log(req.query);
+    // console.log(req.query);
 
     const email = req.query.email;
     const password = req.query.password;
 
     const query= "select count(*) as cnt from users where email='"+email+"' and password='"+password+"';"
     
-    console.log(query);
+    // console.log(query);
 
     db.query(query,(error,result)=>{
         if(error==null){
@@ -126,22 +127,172 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
     blobStream.end(req.file.buffer);
 });
 
-app.post('/api/process-files', (req, res) => {
-    const { email,file_name } = req.body;
+app.get('/api/data', (req, res) => {
+    const { start, end, type } = req.query;
 
-    // Construct the command with the email parameter
-    const pythonCommand = `python ..//services/process_data.py --email ${email} --file ${file_name}`;
+    // Convert UTC dates to Pacific Time and format to date-only strings
+    const localStart = moment(start).tz('America/Los_Angeles').format('YYYY-MM-DD');
+    const localEnd = moment(end).tz('America/Los_Angeles').format('YYYY-MM-DD');
 
-    exec(pythonCommand, (error, stdout, stderr) => {
+    // console.log(`Querying from ${localStart} to ${localEnd}`);
+
+    let selectField = type === 'usage' ? 'units' : 'cost';
+    const query = `SELECT date,start_time, ${selectField} AS value FROM upload_testing WHERE date >= ? AND date <= ?`;
+
+    db.query(query, [localStart, localEnd], (error, results) => {
         if (error) {
-            console.error(`exec error: ${error}`);
-            return res.status(500).send('Error processing files');
+            console.error('Database query error:', error);
+            return res.status(500).send('Error fetching data');
         }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-        res.send('Files processed successfully');
+
+        // console.log('Query results:', results);
+
+        let data = {
+            Morning: 0,
+            Afternoon: 0,
+            Evening: 0,
+            Night: 0
+        };
+
+        results.forEach(row => {
+            const hour = parseInt(row.start_time.split(':')[0]); // Assuming start_time format is 'HH:MM'
+
+            if (hour >= 6 && hour < 12) {
+                data.Morning += parseFloat(row.value);
+            } else if (hour >= 12 && hour < 16) {
+                data.Afternoon += parseFloat(row.value);
+            } else if (hour >= 16 && hour < 20) {
+                data.Evening += parseFloat(row.value);
+            } else {
+                data.Night += parseFloat(row.value);
+            }
+        });
+
+        res.json(data);
     });
 });
+
+app.get('/api/available-dates', (req, res) => {
+    const query = 'SELECT DISTINCT date FROM upload_testing';
+    db.query(query, (error, results) => {
+        if (error) {
+            return res.status(500).send('Error fetching available dates');
+        }
+        const availableDates = results.map(result => result.date); // Assuming 'date' is the column name
+        res.json(availableDates);
+    });
+});
+
+app.get('/api/bar-data', (req, res) => {
+    const { start, end, type } = req.query;
+    let selectField = type === 'usage' ? 'units' : 'cost';
+
+    const localStart = moment(start).tz('America/Los_Angeles').format('YYYY-MM-DD');
+    const localEnd = moment(end).tz('America/Los_Angeles').format('YYYY-MM-DD');
+
+    const query = `SELECT date, start_time, ${selectField} AS value FROM upload_testing  WHERE date >= ? AND date <= ?`;
+
+    db.query(query, [localStart, localEnd], (error, results) => {
+        if (error) {
+            console.error('Database query error:', error);
+            return res.status(500).send('Error fetching data');
+        }
+
+        const barData = {};
+
+        results.forEach(row => {
+            const date = row.date;
+            const hour = parseInt(row.start_time.split(':')[0]);
+            let timeOfDay;
+
+            if (hour >= 6 && hour < 12) {
+                timeOfDay = 'Morning';
+            } else if (hour >= 12 && hour < 16) {
+                timeOfDay = 'Afternoon';
+            } else if (hour >= 16 && hour < 20) { // Adjusted condition for 'Evening'
+                timeOfDay = 'Evening';
+            } else {
+                timeOfDay = 'Night'; // Adjusted condition for 'Night'
+            }
+
+            barData[date] = barData[date] || { Morning: 0, Afternoon: 0, Evening: 0, Night: 0 };
+            barData[date][timeOfDay] += parseFloat(row.value);
+        });
+
+        res.json(barData);
+    });
+});
+
+app.get('/api/scatter-data', (req, res) => {
+    const { start, end } = req.query;
+
+    const startDate = new Date(start).toISOString().split('T')[0];
+    const endDate = new Date(end).toISOString().split('T')[0];
+
+    const query = `SELECT date, units, cost, start_time FROM upload_testing WHERE date >= ? AND date <= ?`;
+
+    db.query(query, [startDate, endDate], (error, results) => {
+        if (error) {
+            console.error('Database query error:', error);
+            return res.status(500).send('Error fetching scatter data');
+        }
+
+        const scatterData = results.map(row => {
+            const hour = new Date(`1970-01-01T${row.start_time}`).getHours();
+            let timeOfDay;
+
+            if (hour >= 6 && hour < 12) {
+                timeOfDay = 'Morning';
+            } else if (hour >= 12 && hour < 16) {
+                timeOfDay = 'Afternoon';
+            } else if (hour >= 16 && hour < 20) {
+                timeOfDay = 'Evening';
+            } else {
+                timeOfDay = 'Night';
+            }
+
+            return {
+                units: row.units,
+                cost: row.cost,
+                date: row.date,
+                timeOfDay: timeOfDay
+            };
+        });
+
+        res.json(scatterData);
+    });
+});
+
+app.get('/api/heatmap-data', (req, res) => {
+    const { type, start, end } = req.query; // 'type' is 'units' or 'cost'
+    let selectField = type === 'usage' ? 'units' : 'cost';
+    const startDate = new Date(start).toISOString().split('T')[0];
+    const endDate = new Date(end).toISOString().split('T')[0];
+
+    // Aggregate data by hour
+    // Extract the hour from start_time and sum up the 'units' or 'cost' for each hour of each day
+    const query = `
+        SELECT 
+            date, 
+            HOUR(start_time) as hour, 
+            SUM(${selectField}) as value 
+        FROM upload_testing 
+        WHERE date >= ? AND date <= ?
+        GROUP BY date, hour
+    `;
+
+    db.query(query, [startDate, endDate], (error, results) => {
+        if (error) {
+            console.error('Error in /api/heatmap-data:', error);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- frontend/src/components/Analysis/Analysis.js
+++ frontend/src/components/Analysis/Analysis.js
@@ -1,11 +1,327 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+import axios from 'axios';
 import './Analysis.css'
 import { redirectIfNotLoggedIn } from '../Auth'
+// import { Chart as ChartJS, , Tooltip, Legend } from 'chart.js';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Bar } from 'react-chartjs-2';
+import { Scatter } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement, PointElement, LineElement } from 'chart.js';
+import HeatMap from 'react-heatmap-grid';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    PointElement,
+    LineElement
+);
+
+// import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Analysis = () => {
     redirectIfNotLoggedIn();
+
+    const [pieData, setPieData] = useState(null);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [availableDates, setAvailableDates] = useState([]);
+    const [dataType, setDataType] = useState('cost'); // 'cost' or 'usage'
+    const [barData, setBarData] = useState({});
+    const [barDataType, setBarDataType] = useState('cost'); // 'cost' or 'usage'
+    // const [barStartDate, setBarStartDate] = useState(new Date());
+    // const [barEndDate, setBarEndDate] = useState(new Date());
+    const [scatterData, setScatterData] = useState({});
+
+    const [heatMapData, setHeatMapData] = useState([]);
+    const [heatMapType, setHeatMapType] = useState('usage')
+    const [xLabels, setXLabels] = useState([]); // Dates
+    const [yLabels, setYLabels] = useState([]); // Time slots
+
+    useEffect(() => {
+        fetchHeatMapData();
+    }, [heatMapType, startDate, endDate]);
+
+    const fetchHeatMapData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/heatmap-data?type=${heatMapType}&start=${startDate.toISOString()}&end=${endDate.toISOString()}`);
+            const transformedData = transformHeatMapData(response.data);
+            setHeatMapData(transformedData.data);
+            setXLabels(transformedData.xLabels);
+            setYLabels(transformedData.yLabels);
+        } catch (error) {
+            console.error('Error fetching heatmap data:', error);
+        }
+    };
+
+    const transformHeatMapData = (data) => {
+        // Assuming each data point is an object with date, hour, and value
+        const xLabels = new Set(); // Unique dates
+        const yLabels = []; // Hours of the day
+        const heatmapData = [];
+    
+        // Generate yLabels for 24 hours
+        for (let i = 0; i < 24; i++) {
+            yLabels.push(`${i}-${i + 1}`);
+        }
+    
+        // Initialize heatmapData with zeros
+        for (let i = 0; i < yLabels.length; i++) {
+            heatmapData.push(Array(24).fill(0)); // Assuming 24 hours in a day
+        }
+    
+        data.forEach(item => {
+            if (item && item.date && item.hour !== undefined) {
+                const dateStr = item.date.split('T')[0]; // Adjust according to your date format
+                xLabels.add(dateStr);
+                const xIndex = Array.from(xLabels).indexOf(dateStr);
+                const hour = parseInt(item.hour);
+                heatmapData[hour][xIndex] = item.value;
+            }
+        });
+    
+        return {
+            data: heatmapData,
+            xLabels: Array.from(xLabels),
+            yLabels: yLabels
+        };
+    };
+
+    const fetchScatterData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/scatter-data?start=${startDate.toISOString()}&end=${endDate.toISOString()}`);
+            // console.log ("Scatter Data from backend:", response.data); // Log backend data
+            setScatterData(transformScatterData(response.data));
+        } catch (error) {
+            console.error('Error fetching scatter plot data:', error);
+        }
+    };
+    
+    const transformScatterData = (data) => {
+        const timePeriods = {
+            Morning: [],
+            Afternoon: [],
+            Evening: [],
+            Night: []
+        };
+    
+        data.forEach(item => {
+            if (timePeriods[item.timeOfDay]) {
+                timePeriods[item.timeOfDay].push({ x: item.units, y: item.cost, date: item.date });
+            }
+        });
+    
+        return {
+            datasets: [
+                {
+                    label: 'Morning ',
+                    data: timePeriods.Morning,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)' // Color for Morning
+                },
+                {
+                    label: 'Afternoon',
+                    data: timePeriods.Afternoon,
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)' // Color for Afternoon
+                },
+                {
+                    label: 'Evening',
+                    data: timePeriods.Evening,
+                    backgroundColor: 'rgba(255, 206, 86, 0.5)' // Color for Evening
+                },
+                {
+                    label: 'Night',
+                    data: timePeriods.Night,
+                    backgroundColor: 'rgba(75, 192, 192, 0.5)' // Color for Night
+                }
+            ]
+        };
+    };
+    
+    useEffect(() => {
+        fetchScatterData();
+    }, [startDate, endDate]);
+
+    useEffect(() => {
+        fetchBarData();
+    }, [startDate,endDate,barDataType]);
+
+    const fetchBarData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/bar-data?start=${startDate.toISOString()}&end=${endDate.toISOString()}&type=${dataType}`);
+            setBarData(response.data);
+        } catch (error) {
+            console.error('Error fetching bar chart data:', error);
+        }
+    };
+
+    const generateBarChartData = () => {
+        // Assuming barData is an object where each key is a date and each value is an object with Morning, Afternoon, Evening, Night values
+        const chartLabels = Object.keys(barData);
+        const morningData = chartLabels.map(label => barData[label].Morning || 0);
+        const afternoonData = chartLabels.map(label => barData[label].Afternoon || 0);
+        const eveningData = chartLabels.map(label => barData[label].Evening || 0);
+        const nightData = chartLabels.map(label => barData[label].Night || 0);
+        // console.log(barData)
+        return {
+            labels: chartLabels,
+            datasets: [
+                {
+                    label: 'Morning',
+                    data: morningData,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                    label: 'Afternoon',
+                    data: afternoonData,
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                },
+                {
+                    label: 'Evening',
+                    data: eveningData,
+                    backgroundColor: 'rgba(255, 206, 86, 0.5)',
+                },
+                {
+                    label: 'Night',
+                    data: nightData,
+                    backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                }
+            ]
+        };
+    };
+
+    useEffect(() => {
+        fetchAvailableDates();
+    }, []);
+
+    const fetchAvailableDates = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/available-dates');
+            const dates = response.data.map(dateStr => new Date(dateStr));
+            setAvailableDates(dates);
+    
+            if (dates.length > 0) {
+                // Find the earliest and latest dates
+                const earliestDate = new Date(Math.min(...dates));
+                const latestDate = new Date(Math.max(...dates));
+                setStartDate(earliestDate);
+                setEndDate(latestDate);
+                fetchData(earliestDate, latestDate); // Fetch data for the full range
+            }
+        } catch (error) {
+            console.error('Error fetching available dates:', error);
+        }
+    };
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/data?start=${startDate.toISOString()}&end=${endDate.toISOString()}&type=${dataType}`);
+                const data = response.data;
+                // console.log(data);
+                setPieData({
+                    labels: Object.keys(data),
+                    datasets: [{
+                        data: Object.values(data),
+                        backgroundColor: [
+                            '#87CEEB', // Gold for Morning
+                            '#FFA500', // Orange for Noon
+                            '#FF4500', // Orange Red for Evening
+                            '#000080', // Navy for Night
+                        ],
+                        borderColor: [
+                            '#FFFFFF', // White borders for all
+                            '#FFFFFF',
+                            '#FFFFFF',
+                            '#FFFFFF',
+                        ],
+                        borderWidth: 1,
+                    }],
+                });
+            } catch (error) {
+                console.error(`Error fetching ${dataType} data:`, error);
+            }
+        };
+    
+    useEffect(() => {
+        fetchData();
+    }, [startDate, endDate, dataType]);
+
     return (
-        <div>Analysis</div>
+        <div className='container-analysis'>
+            <div className='filter'>
+                <DatePicker
+                    selected={startDate}
+                    onChange={date => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    includeDates={availableDates}
+                />
+                <DatePicker
+                    selected={endDate}
+                    onChange={date => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    includeDates={availableDates}
+                />
+            </div>
+            <div className='tp'>
+                <div>
+                    <button onClick={() => setDataType('cost')}>Show Cost</button>
+                    <button onClick={() => setDataType('usage')}>Show Usage</button>
+                </div>
+                Pie Chart
+                {pieData && <Pie data={pieData} />}
+            </div>
+            <div className='tp'>
+                {/* Scatter Plot */}
+                {scatterData && scatterData.datasets && scatterData.datasets.length > 0 ? (
+                    <Scatter data={scatterData} options={{
+                        scales: {
+                            x: { type: 'linear', position: 'bottom', title: { display: true, text: 'Energy Consumption' } },
+                            y: { title: { display: true, text: 'Cost' } }
+                        }
+                    }} />
+                ) : (
+                    <p>Loading scatter plot data...</p>
+                )}
+            </div>
+            <div className='tp'>
+                <div>
+                    <button onClick={() => setHeatMapType('usage')}>Show Usage</button>
+                    <button onClick={() => setHeatMapType('cost')}>Show Cost</button>
+                </div>
+                <HeatMap
+                    xLabels={xLabels}
+                    yLabels={yLabels}
+                    data={heatMapData}
+                    // ... other heatmap properties as needed ...
+                />
+            </div>
+            <div className='bp-1'>
+                <div>
+                    <button onClick={() => setBarDataType('cost')}>Show Cost</button>
+                    <button onClick={() => setBarDataType('usage')}>Show Usage</button>
+                </div>
+                Stacked Bar Chart
+                <Bar data={generateBarChartData()} options={{ 
+                    scales: { x: { stacked: true }, y: { stacked: true } }
+                }} />
+            </div>
+            <div className='bp-2'>
+                Table with individual colum search feature
+            </div>
+        </div>
     )
 }
 
